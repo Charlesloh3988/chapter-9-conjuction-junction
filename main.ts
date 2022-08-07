7206e5e36d1a696a0fa4fd9cc5da40d1287aba88
Charlesloh3988 <110405821+Charlesloh3988@users.noreply.github.com>
2022-08-07 15:06:33 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,21 +1,60 @@
 let junction = 0
 basic.forever(function () {
     reromicro.ReadLineSensors()
-    if (reromicro.LineSensorDetectsLine(LineSensors.Left) && (reromicro.LineSensorDetectsLine(LineSensors.Center) && reromicro.LineSensorDetectsLine(LineSensors.Right))) {
-        junction += 1
-        basic.pause(100)
+    if (reromicro.ReadUltrasonic() <= 15) {
         reromicro.Brake()
-        music.playTone(262, music.beat(BeatFraction.Half))
-        basic.showNumber(junction)
-    } else if (reromicro.LineSensorDetectsLine(LineSensors.Center)) {
-        reromicro.MoveForward(35)
-    } else if (reromicro.LineSensorDetectsLine(LineSensors.Left)) {
-        reromicro.RunMotor(Motors.Left, 0)
-        reromicro.RunMotor(Motors.Right, 35)
-    } else if (reromicro.LineSensorDetectsLine(LineSensors.Right)) {
-        reromicro.RunMotor(Motors.Left, 35)
-        reromicro.RunMotor(Motors.Right, 0)
     } else {
-        reromicro.Brake()
+        if (reromicro.LineSensorDetectsLine(LineSensors.Left) && (reromicro.LineSensorDetectsLine(LineSensors.Center) && reromicro.LineSensorDetectsLine(LineSensors.Right))) {
+            junction += 1
+            basic.pause(100)
+            reromicro.Brake()
+            basic.showNumber(junction)
+            while (junction >= 5) {
+                for (let index = 0; index < 1; index++) {
+                    reromicro.Brake()
+                    music.setVolume(183)
+                    music.setTempo(165)
+                    music.playTone(196, music.beat(BeatFraction.Half))
+                    music.playTone(262, music.beat(BeatFraction.Half))
+                    music.playTone(330, music.beat(BeatFraction.Half))
+                    music.playTone(392, music.beat(BeatFraction.Half))
+                    music.playTone(523, music.beat(BeatFraction.Half))
+                    music.playTone(659, music.beat(BeatFraction.Half))
+                    music.playTone(784, music.beat(BeatFraction.Whole))
+                    music.playTone(659, music.beat(BeatFraction.Whole))
+                    for (let index = 0; index < 1; index++) {
+                        music.playTone(208, music.beat(BeatFraction.Half))
+                        music.playTone(262, music.beat(BeatFraction.Half))
+                        music.playTone(311, music.beat(BeatFraction.Half))
+                        music.playTone(415, music.beat(BeatFraction.Half))
+                        music.playTone(523, music.beat(BeatFraction.Half))
+                        music.playTone(622, music.beat(BeatFraction.Half))
+                        music.playTone(831, music.beat(BeatFraction.Whole))
+                        music.playTone(622, music.beat(BeatFraction.Whole))
+                    }
+                    for (let index = 0; index < 1; index++) {
+                        music.playTone(233, music.beat(BeatFraction.Half))
+                        music.playTone(294, music.beat(BeatFraction.Half))
+                        music.playTone(349, music.beat(BeatFraction.Half))
+                        music.playTone(466, music.beat(BeatFraction.Half))
+                        music.playTone(587, music.beat(BeatFraction.Half))
+                        music.playTone(698, music.beat(BeatFraction.Half))
+                        music.playTone(932, music.beat(BeatFraction.Whole))
+                        music.playTone(932, music.beat(BeatFraction.Half))
+                        music.playTone(932, music.beat(BeatFraction.Half))
+                        music.playTone(932, music.beat(BeatFraction.Half))
+                        music.playTone(1046, music.beat(BeatFraction.Double))
+                    }
+                }
+            }
+        } else if (reromicro.LineSensorDetectsLine(LineSensors.Center)) {
+            reromicro.MoveForward(35)
+        } else if (reromicro.LineSensorDetectsLine(LineSensors.Left)) {
+            reromicro.RunMotor(Motors.Left, 0)
+            reromicro.RunMotor(Motors.Right, 35)
+        } else if (reromicro.LineSensorDetectsLine(LineSensors.Right)) {
+            reromicro.RunMotor(Motors.Left, 35)
+            reromicro.RunMotor(Motors.Right, 0)
+        }
     }
 })
